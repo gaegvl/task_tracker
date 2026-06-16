d0d7f06542ad4f37c04f3369b8f40e752a3ec18e
Egor Gavrilov <Egor4it@yandex.ru>
2026-06-16 13:31:29 +0500

    Удаление дублирующего теста

--- tests/test_delete_task_use_case.py
+++ tests/test_delete_task_use_case.py
@@ -14,22 +14,6 @@ from tests.helpers import create_project_in_memory, create_task_in_memory
 
 
 @pytest.mark.asyncio
-async def test_delete_task_use_case_removes_task_from_repository() -> None:
-    project_repository = InMemoryProjectRepository()
-    project_id = await create_project_in_memory(project_repository)
-    task_repository = InMemoryTaskRepository()
-    task_id = await create_task_in_memory(
-        task_repository, project_repository, project_id
-    )
-
-    delete_use_case = DeleteTaskUseCase(task_repository)
-    await delete_use_case.execute(command=DeleteTaskCommand(task_id=task_id))
-
-    with pytest.raises(TaskNotFoundError):
-        await task_repository.get_by_id(task_id)
-
-
-@pytest.mark.asyncio
 async def test_delete_task_use_case_not_found() -> None:
     repository = InMemoryTaskRepository()
     delete_task = DeleteTaskUseCase(repository)
